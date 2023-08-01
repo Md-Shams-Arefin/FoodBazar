fbdd61dd3f6d28e4c7f271698d6d9fa6a03c6cb6
arefins56 <arefins56@penguin>
2023-08-01 21:42:31 +0600

    updated

--- core/settings.py
+++ core/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-jud&&e#9uqkc6na2ecw2rv6^_k7ny*w9$i%#8pzgumdy-8@mt7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse('postgres://myuser:moBF22ZZcXtsx0jLf9yAO9RbSxRGf8Yd@dpg-cj2co86nqql8v0c9v8qg-a.singapore-postgres.render.com/djangodb_amtm')
 }
 
 
