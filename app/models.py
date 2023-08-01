91e0ea1d0ca77ff48ba7ef47019a3e0078882231
arefins56 <arefins56@penguin>
2023-08-01 18:54:12 +0600

    done

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Book_a_table(models.Model):
     phone = models.CharField(max_length=50, null=True)
     date = models.DateField(null=True)
     time = models.TimeField(null=True)
-    people = models.CharField(null=True)
+    people = models.CharField(max_length=50,null=True)
     message = models.TextField(null=True)
 
     def __str__(self):
